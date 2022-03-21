31f33991d6569a5f37d7c3e03ec72e9b90c5c709
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2022-03-21 08:20:14 +0200

    🚀 add test cases, when input have no multiples of 3 or 5

--- Multiples of 3 or 5/index.test.js
+++ Multiples of 3 or 5/index.test.js
@@ -8,4 +8,10 @@ describe("Multiples of 3 or 5", function () {
       assert.strictEqual(actual, 23)
     })
   }
+  {
+    let actual = solution(2)
+    it.only(`Expected ${23}, got ${actual}`, () => {
+      assert.strictEqual(actual, 0)
+    })
+  }
 })

ddd30af33a76358b91a28eeead538d7a34e545e3
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2022-03-21 08:19:21 +0200

    test Multiples of 3 or 5

---
+++ Multiples of 3 or 5/index.test.js
@@ -0,0 +1,11 @@
+const { assert } = require("chai")
+const solution = require("./index");
+
+describe("Multiples of 3 or 5", function () {
+  {
+    let actual = solution(10)
+    it.only(`Expected ${23}, got ${actual}`, () => {
+      assert.strictEqual(actual, 23)
+    })
+  }
+})
